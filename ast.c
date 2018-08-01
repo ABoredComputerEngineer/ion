085615b5d3618d3c9138e748e0fd75f9ee995210
Asim Maharjan <maharjan291@gmail.com>
2018-08-01 21:26:56 +0545

    Split ast printing into ast.h and ast.c

--- ast.c
+++ ast.c
@@ -1,282 +1,4 @@
-typedef struct Decl Decl;
-typedef struct Expr Expr;
-typedef struct Stmt Stmt;
-typedef enum TypeSpecKind TypeSpecKind;
-typedef struct TypeSpec TypeSpec;
-
-enum TypeSpecKind {
-     TYPESPEC_NAME,
-     TYPESPEC_PAREN,
-     TYPESPEC_FUNC,
-     TYPESPEC_ARRAY,
-     TYPESPEC_POINTER
-};
-
-typedef struct Func_TypeSpec{
-     struct{
-          BUF( TypeSpec **args;) // Buffer to hold the data type of arguments given to a function ) )
-          size_t num_args;
-     };
-     TypeSpec *ret_type; // The return type of the function
-} Func_TypeSpec;
-
-struct TypeSpec{
-     TypeSpecKind kind;
-     struct {
-          const char *name; // Type name
-          // array  and pointer
-          struct {
-               TypeSpec *base_type; // Base type of an array
-               Expr *size; // Index of the array
-          };
-          Func_TypeSpec func_decl;
-     };
-};
-
-
-typedef enum DeclKind{
-     DECL_NONE,
-     DECL_ENUM,
-     DECL_STRUCT,
-     DECL_UNION,
-     DECL_VAR,
-     DECL_CONST,
-     DECL_TYPEDEF,
-     DECL_FUNC
-} DeclKind;
-
-typedef enum ExprKind {
-     EXPR_NONE,
-     EXPR_INT,
-     EXPR_FLOAT,
-     EXPR_STR,
-     EXPR_NAME,
-     EXPR_CAST,
-     EXPR_CALL, // Function calls
-     EXPR_INDEX,
-     EXPR_FIELD,
-     EXPR_UNARY,
-     EXPR_BINARY,
-     EXPR_TERNARY,
-     EXPR_COMPOUND
-
-} ExprKind;
-
-typedef enum StmtKind {
-     STMT_NONE,
-     STMT_AUTO_ASSIGN,
-     STMT_OP_ASSIGN,
-     STMT_FOR,
-     STMT_WHILE,
-     STMT_DO_WHILE,
-     STMT_IF,
-     STMT_SWITCH,
-     STMT_RETURN,
-     STMT_BREAK,
-     STMT_CONTINUE
-} StmtKind;
-
-typedef struct enum_item{
-     const char *name;
-     Expr *expr;
-     TypeSpec *type;
-} enum_item;
-
-typedef struct aggregate_item{
-     BUF(const char **name_list); // Buffer for list of names of aggregate types e.g. x,y = int or whatever
-     size_t num_names;
-     TypeSpec *type;
-} aggregate_item;
-
-typedef struct func_param {
-     const char *name;
-     TypeSpec *type;
-} func_param;
-
-typedef struct func_def{
-     BUF(func_param **param_list); // Buffer for storing function parameter list
-     size_t num_params;
-     TypeSpec *ret_type;  // return type of the function
-} func_def;
-
-typedef struct enum_def{
-     size_t num_enum_items;
-     enum_item **enum_items; // Buffer for storing items of enum declaration,e.g. ( name : expr )
-} enum_def;
-
-typedef struct aggregate_def{
-     size_t num_aggregate_items;
-     aggregate_item **aggregate_items; // Buffer to store the name value pairs for structures and unions``
-} aggregate_def;
-
-typedef struct typedef_def{
-     TypeSpec *type;
-//     Expr *expr; // MOD: Removed expr type for typedef declarations
-} typedef_def;
-
-typedef struct const_def{
-     Expr *expr;
-} const_def;
-
-typedef struct var_def {
-     TypeSpec *type;
-     Expr *expr;
-} var_def;
-
-struct Decl{
-     DeclKind kind;
-     const char *name;
-     union {
-          enum_def enum_decl;
-          aggregate_def aggregate_decl;
-          typedef_def typedef_decl;
-          const_def const_decl;
-          var_def var_decl;
-          func_def func_decl;
-     };
-};
-
-
-// 
-//
-typedef struct unary_def {
-     Expr *operand;
-} unary_def;
-
-typedef struct field_def{
-     Expr *operand;
-     const char *field_name;
-} field_def;
-
-typedef struct array_def{
-     Expr *operand;
-     Expr *index;
-} array_def;
-
-typedef struct func_call_def{
-     Expr *operand;
-     size_t num_args;
-     Expr **args;
-} func_call_def;
-
-typedef struct binary_def{
-     Expr *left;
-     Expr *right;
-} binary_def;
-
-typedef struct ternary_def {
-     Expr *cond_expr;
-     Expr *then_expr;
-     Expr *else_expr;
-} ternary_def;
-
-typedef struct compound_def{
-     TypeSpec *type;
-     size_t num_args;
-     BUF(Expr **args); // Buffer
-} compound_def;
-
-typedef struct cast_def{
-     TypeSpec *cast_type;
-     Expr *expr;
-} cast_def;
-struct Expr {
-     ExprKind kind;
-     TokenKind op;
-     union {
-          uint64_t int_val;
-          double float_val;
-          const char *str_val;
-          const char *name;
-          unary_def unary_expr;
-          array_def array_expr;
-          field_def field_expr;
-
-          binary_def binary_expr;
-          ternary_def ternary_expr;
-          
-          cast_def cast_expr;
-          compound_def compound_expr;
-
-          func_call_def func_call_expr;
-     };
-};
-
-typedef struct StmtBlock {
-     size_t num_stmts;
-     BUF(Stmt **stmts); // Buffer to hold a list of statements
-} StmtBlock;
-
-
-typedef struct Elseif {
-     Expr *cond;
-     StmtBlock elseif_block;
-} Elseif;
-
-typedef struct stmt_if_def {
-     Expr *cond;  // the condition for if statement
-     size_t num_elseifs;
-     Elseif *elseifs; // list of pointers to else_if blocks
-     StmtBlock else_block;
-} stmt_if_def;
-
-typedef struct stmt_auto_assign_def { // Colon assign
-     const char *name;
-     Expr *rhs;
-} stmt_auto_assign_def;
-
-typedef struct stmt_op_assign_def {
-     Expr *lhs;
-     TokenKind op;
-//     const char *name;
-     Expr *rhs;
-}  stmt_op_assign_def;
-
-typedef struct Case {
-     Expr *case_expr; // buffer to hold a list of expressions
-     StmtBlock case_block;
-} Case;
-
-typedef struct stmt_switch_def{
-     Expr *expr;
-     StmtBlock default_block;
-     size_t num_cases;
-    BUF( Case *cases;) // buffer to hold all the case statements;
-} stmt_switch_def;
-
-typedef struct stmt_for_def{
-     Expr *expr_init;
-     Expr *expr_update;
-     Expr *expr_cond;
-} stmt_for_def;
-
-typedef struct stmt_return_def {
-     Expr *return_expr;
-} stmt_return_def;
-
-typedef struct stmt_while_def {
-     Expr *expr_cond;
-} stmt_while_def;
-
-struct Stmt{
-     StmtKind kind;
-//     Expr *expr; //  for statements like for, while ,return, switch do while etc, cond expression for if .
-     StmtBlock block; // Statements block for for, while... statements, if block for if statement.
-
-     union {
-          stmt_return_def return_stmt;
-          stmt_while_def while_stmt;
-                    
-          stmt_if_def if_stmt;
-          stmt_for_def for_stmt;
-          stmt_switch_def switch_stmt;
-          stmt_auto_assign_def auto_assign_stmt;
-          stmt_op_assign_def op_assign_stmt;
-     };
-
-};
-
-
+#include "ast.h"
 // Expressions constructors [exprcon]
 Expr *expr_int( uint64_t int_val );
 Expr *expr_float( double float_val);
@@ -501,7 +223,7 @@ void print_stmt(Stmt *stmt){
                     printf("(if ");
                     print_expr(tmp.cond);
                     printf("\n(then ");
-                    print_stmt_block(stmt->block,indent++);
+                    print_stmt_block(stmt->if_stmt.if_block,indent++);
                     printf(")\n");
                     indent--;
                     if ( tmp.elseifs != NULL ){
@@ -542,37 +264,37 @@ void print_stmt(Stmt *stmt){
                printf(")");
                //printf("\n");
                //printf("(");
-               print_stmt_block(stmt->block,indent++);
+               print_stmt_block(stmt->for_stmt.block,indent++);
                printf(")");
                indent--;
                break;
           case STMT_WHILE:
                printf("( while ");
                print_expr(stmt->while_stmt.expr_cond);
-               print_stmt_block(stmt->block,indent++);
+               print_stmt_block(stmt->while_stmt.block,indent++);
                printf(")");
                indent--;
                break;
           case STMT_DO_WHILE:
                printf("( do ");
-               print_stmt_block(stmt->block,indent++);
+               print_stmt_block(stmt->while_stmt.block,indent++);
                printf(") while (");
                print_expr(stmt->while_stmt.expr_cond);
                printf(")"); 
                indent--;
                break;   
-          case STMT_AUTO_ASSIGN:
-               printf("( %s := ",stmt->auto_assign_stmt.name);
-               print_expr( stmt->auto_assign_stmt.rhs); 
+          case STMT_ASSIGN:
+               printf("( %s := ",stmt->assign_stmt.name);
+               print_expr( stmt->assign_stmt.rhs); 
                printf(")");
                break;
-          case STMT_OP_ASSIGN:
-               printf("( ");
-               print_expr(stmt->op_assign_stmt.lhs);
-               printf(" = " );
-               print_expr(stmt->op_assign_stmt.rhs);
-               printf(" )");
-               break;
+        //  case STMT_OP_ASSIGN:
+        //       printf("( ");
+        //       print_expr(stmt->op_assign_stmt.lhs);
+        //       printf(" = " );
+        //       print_expr(stmt->op_assign_stmt.rhs);
+        //       printf(" )");
+        //       break;
           case STMT_RETURN:
                printf("( return ");
                print_expr(stmt->return_stmt.return_expr);
@@ -596,19 +318,30 @@ void print_stmt(Stmt *stmt){
                     print_expr(tmp.expr);
                     
                     for ( Case *it = tmp.cases; it!= tmp.cases + tmp.num_cases; it++){
-                         
-                         printf("\n(case ");
-                         print_expr( (it)->case_expr );
+                         if ( !(it->isdefault) ){     
+                              printf("\n(case ");
+                              for ( Expr **ie = it->expr_list; ie!=it->expr_list+it->num_expr; ie++){
+                                   
+                                   print_expr( *ie );
+                                   printf(" ");
+                              }
+                         } else {
+                              printf("\n(default ");
+                         }
                          print_stmt_block( (it)->case_block, indent++ );
                          indent--;
                          printf("\nbreak)");
                     }
-                    printf("\n(default ");
-                    print_stmt_block(  tmp.default_block, indent++ );
                     indent--;
                     printf("))");
                     break;
                }
+
+          case STMT_EXPR:
+               printf("( ");
+               print_expr(stmt->expr_stmt);
+               printf(")");
+               break;
           default:
                assert(0);
                break;
@@ -960,10 +693,9 @@ StmtBlock new_block(size_t num_stmts, Stmt **stmts){
      
 }
 
-Stmt *new_stmt(StmtKind kind, StmtBlock block ){
+Stmt *new_stmt(StmtKind kind){
      Stmt *new = xcalloc(1,sizeof(Stmt));
      new->kind = kind;
-     new->block = block;
     return new; 
 }
 
@@ -984,7 +716,8 @@ Elseif *elseif_list(size_t num, ... ){
 }
 
 Stmt *stmt_if( Expr *condition,StmtBlock if_block,size_t num_elseifs, Elseif *elseifs, StmtBlock else_block ){
-     Stmt *new= new_stmt(STMT_IF,if_block);
+     Stmt *new= new_stmt(STMT_IF);
+     new->if_stmt.if_block = if_block;
      new->if_stmt.cond = condition;
      new->if_stmt.elseifs = elseifs;
      new->if_stmt.num_elseifs = num_elseifs;
@@ -1006,66 +739,66 @@ Stmt *stmt_if( Expr *condition,StmtBlock if_block,size_t num_elseifs, Elseif *el
 //     STMT_CONTINUE
 //} StmtKind;
 
-Stmt *stmt_auto_assign(const char *name, Expr *expr){
-     Stmt *new = new_stmt(STMT_AUTO_ASSIGN,(StmtBlock){0,NULL});
-     new->auto_assign_stmt.rhs = expr;
-     new->auto_assign_stmt.name = name;
-     return new;
-}
-Stmt *stmt_op_assign(Expr *lhs, TokenKind op, Expr *rhs){
-     Stmt *new = new_stmt(STMT_OP_ASSIGN,(StmtBlock){0,NULL});
-     new->op_assign_stmt.lhs = lhs;
-     new->op_assign_stmt.op = op;
-     new->op_assign_stmt.rhs = rhs;
+Stmt *stmt_assign(const char *name, Expr *expr){
+     Stmt *new = new_stmt(STMT_ASSIGN);
+     new->assign_stmt.rhs = expr;
+     new->assign_stmt.name = name;
      return new;
 }
 Stmt *stmt_for(Expr *init, Expr *cond, Expr *update, StmtBlock block){
-     Stmt *new = new_stmt(STMT_FOR,block);
+     Stmt *new = new_stmt(STMT_FOR);
+     new->for_stmt.block = block;
      new->for_stmt.expr_init = init;
      new->for_stmt.expr_cond = cond;
      new->for_stmt.expr_update = update;
      return new;
 }
 Stmt *stmt_while(Expr *cond, StmtBlock block){
-     Stmt *new = new_stmt(STMT_WHILE,block);
+     Stmt *new = new_stmt(STMT_WHILE);
+     new->while_stmt.block = block;
      new->while_stmt.expr_cond = cond;
      return new;
 }
 
 Stmt *stmt_do_while(Expr *cond, StmtBlock block){
-     Stmt *new = new_stmt(STMT_DO_WHILE,block);
+     Stmt *new = new_stmt(STMT_DO_WHILE);
+     new->while_stmt.block = block;
      new->while_stmt.expr_cond = cond;
      return new;
 }
 
-Case new_case( Expr *expr, StmtBlock block ){
-     return (Case){expr,block};
+Case new_case( Expr **expr,size_t num_expr, bool isdefault, StmtBlock block ){
+     return (Case){expr,num_expr,isdefault,block};
 }
 
-Stmt *stmt_switch(Expr *expr, size_t num_cases, Case *cases, StmtBlock default_block){
-     Stmt *new = new_stmt(STMT_SWITCH,default_block);
+Stmt *stmt_switch(Expr *expr, size_t num_cases, Case *cases){
+     Stmt *new = new_stmt(STMT_SWITCH);
      new->switch_stmt.expr = expr;
      new->switch_stmt.num_cases = num_cases;
      new->switch_stmt.cases = cases;
-     new->switch_stmt.default_block = default_block;
      return new;
 }
 Stmt *stmt_return(Expr *expr){
-     Stmt *new = new_stmt(STMT_RETURN, (StmtBlock){0,NULL});
+     Stmt *new = new_stmt(STMT_RETURN);
      new->return_stmt.return_expr =  expr;
      return new;
 }
 
 
 Stmt *stmt_break(void){
-     return new_stmt(STMT_BREAK, (StmtBlock){0,NULL } );
+     return new_stmt(STMT_BREAK);
 }
 
 Stmt *stmt_continue(void){
-     return new_stmt(STMT_CONTINUE,(StmtBlock){0,NULL});
+     return new_stmt(STMT_CONTINUE);
 }
 
+Stmt *stmt_expr(Expr *expr){
+     Stmt *new = new_stmt(STMT_EXPR);
+     new->expr_stmt = expr;
+     return new;
 
+}
 
 // Test Function [tstfunc]
 //
@@ -1112,27 +845,28 @@ void ast_decl_test(){
 
 void ast_stmt_test(){
 
-     Stmt **stmt1 = stmt_list(3,stmt_break(),stmt_op_assign(expr_name("i"),'=',expr_name("x")),stmt_while(expr_binary(EQ,expr_name("ab"),expr_int(1)),new_block(1,stmt_list(1,stmt_continue()))));
+     Stmt **stmt1 = stmt_list(3,stmt_break(),stmt_expr(expr_name("i")),stmt_while(expr_binary(EQ,expr_name("ab"),expr_int(1)),new_block(1,stmt_list(1,stmt_continue()))));
      
-
-     Stmt **stmt2 = stmt_list(2,stmt_return(expr_int(1)), stmt_for(expr_binary('=',expr_name("i"),expr_int(1)),expr_binary(LTEQ,expr_name("i"),expr_int(10)),expr_binary('+',expr_name("i"),expr_int(1)),new_block(1,stmt_list(1,stmt_continue()))));     
+     Stmt **stmt2 = stmt_list(3, stmt_return(expr_name("true")),stmt_expr(expr_binary(LTEQ,expr_name("a"),expr_int(3))),stmt_continue());
      StmtBlock block_test1 = new_block(3,stmt1 );
 
      StmtBlock block_test2 = new_block(2,stmt2);
      Elseif *elifs = elseif_list(1,new_elif(expr_int(1),block_test1));
 
-     Case *cases = case_list(2, new_case(expr_int(1),block_test1), new_case(expr_int(2),block_test2));
+     Case *cases = case_list(2, new_case(expr_list(2,expr_int(1),expr_name("a")),2,false,block_test1), new_case(expr_list(1,expr_int(2)),1,true,block_test2));
      
      Stmt *stmt_list[] = {
           stmt_if(expr_binary(LSHIFT_ASSIGN,expr_int(1),expr_int(2)),block_test1,1,elifs,block_test1),
           stmt_while(expr_binary(LTEQ,expr_name("abc"),expr_int(12)), block_test1),
           stmt_do_while(expr_binary(LTEQ,expr_name("c"),expr_int(15)), block_test2),
-          stmt_switch(expr_name("a"),2,cases,block_test2),
-          stmt_auto_assign("a",expr_int(123))
+          stmt_switch(expr_name("a"),2,cases),
+          stmt_assign("a",expr_int(123))
      };
 //     print_stmt_block(block_test);
 //     print_stmt(stmt_if(expr_binary(LSHIFT_ASSIGN,expr_int(1),expr_int(2)),block_test,0,NULL,block_test));
      putchar('\n');
+
+
 
      for ( Stmt **it = stmt_list; it != stmt_list + sizeof(stmt_list)/sizeof(Stmt *); it++ ){
           print_stmt(*it);
---
+++ ast.h
@@ -0,0 +1,284 @@
+
+typedef struct Decl Decl;
+typedef struct Expr Expr;
+typedef struct Stmt Stmt;
+typedef enum TypeSpecKind TypeSpecKind;
+typedef struct TypeSpec TypeSpec;
+
+enum TypeSpecKind {
+     TYPESPEC_NAME,
+     TYPESPEC_PAREN,
+     TYPESPEC_FUNC,
+     TYPESPEC_ARRAY,
+     TYPESPEC_POINTER
+};
+
+typedef struct Func_TypeSpec{
+     struct{
+          BUF( TypeSpec **args;) // Buffer to hold the data type of arguments given to a function ) )
+          size_t num_args;
+     };
+     TypeSpec *ret_type; // The return type of the function
+} Func_TypeSpec;
+
+struct TypeSpec{
+     TypeSpecKind kind;
+     struct {
+          const char *name; // Type name
+          // array  and pointer
+          struct {
+               TypeSpec *base_type; // Base type of an array
+               Expr *size; // Index of the array
+          };
+          Func_TypeSpec func_decl;
+     };
+};
+
+
+typedef enum DeclKind{
+     DECL_NONE,
+     DECL_ENUM,
+     DECL_STRUCT,
+     DECL_UNION,
+     DECL_VAR,
+     DECL_CONST,
+     DECL_TYPEDEF,
+     DECL_FUNC
+} DeclKind;
+
+typedef enum ExprKind {
+     EXPR_NONE,
+     EXPR_INT,
+     EXPR_FLOAT,
+     EXPR_STR,
+     EXPR_NAME,
+     EXPR_CAST,
+     EXPR_CALL, // Function calls
+     EXPR_INDEX,
+     EXPR_FIELD,
+     EXPR_UNARY,
+     EXPR_BINARY,
+     EXPR_TERNARY,
+     EXPR_COMPOUND
+
+} ExprKind;
+
+typedef enum StmtKind {
+     STMT_NONE,
+     STMT_ASSIGN,
+     STMT_EXPR,
+     STMT_FOR,
+     STMT_WHILE,
+     STMT_DO_WHILE,
+     STMT_IF,
+     STMT_SWITCH,
+     STMT_RETURN,
+     STMT_BREAK,
+     STMT_CONTINUE,
+     STMT_BLOCK
+} StmtKind;
+
+typedef struct enum_item{
+     const char *name;
+     Expr *expr;
+     TypeSpec *type;
+} enum_item;
+
+typedef struct aggregate_item{
+     BUF(const char **name_list); // Buffer for list of names of aggregate types e.g. x,y = int or whatever
+     size_t num_names;
+     TypeSpec *type;
+} aggregate_item;
+
+typedef struct func_param {
+     const char *name;
+     TypeSpec *type;
+} func_param;
+
+typedef struct func_def{
+     BUF(func_param **param_list); // Buffer for storing function parameter list
+     size_t num_params;
+     TypeSpec *ret_type;  // return type of the function
+} func_def;
+
+typedef struct enum_def{
+     size_t num_enum_items;
+     enum_item **enum_items; // Buffer for storing items of enum declaration,e.g. ( name : expr )
+} enum_def;
+
+typedef struct aggregate_def{
+     size_t num_aggregate_items;
+     aggregate_item **aggregate_items; // Buffer to store the name value pairs for structures and unions``
+} aggregate_def;
+
+typedef struct typedef_def{
+     TypeSpec *type;
+//     Expr *expr; // MOD: Removed expr type for typedef declarations
+} typedef_def;
+
+typedef struct const_def{
+     Expr *expr;
+} const_def;
+
+typedef struct var_def {
+     TypeSpec *type;
+     Expr *expr;
+} var_def;
+
+struct Decl{
+     DeclKind kind;
+     const char *name;
+     union {
+          enum_def enum_decl;
+          aggregate_def aggregate_decl;
+          typedef_def typedef_decl;
+          const_def const_decl;
+          var_def var_decl;
+          func_def func_decl;
+     };
+};
+
+
+// 
+//
+typedef struct unary_def {
+     Expr *operand;
+} unary_def;
+
+typedef struct field_def{
+     Expr *operand;
+     const char *field_name;
+} field_def;
+
+typedef struct array_def{
+     Expr *operand;
+     Expr *index;
+} array_def;
+
+typedef struct func_call_def{
+     Expr *operand;
+     size_t num_args;
+     Expr **args;
+} func_call_def;
+
+typedef struct binary_def{
+     Expr *left;
+     Expr *right;
+} binary_def;
+
+typedef struct ternary_def {
+     Expr *cond_expr;
+     Expr *then_expr;
+     Expr *else_expr;
+} ternary_def;
+
+typedef struct compound_def{
+     TypeSpec *type;
+     size_t num_args;
+     BUF(Expr **args); // Buffer
+} compound_def;
+
+typedef struct cast_def{
+     TypeSpec *cast_type;
+     Expr *expr;
+} cast_def;
+struct Expr {
+     ExprKind kind;
+     TokenKind op;
+     union {
+          uint64_t int_val;
+          double float_val;
+          const char *str_val;
+          const char *name;
+          unary_def unary_expr;
+          array_def array_expr;
+          field_def field_expr;
+
+          binary_def binary_expr;
+          ternary_def ternary_expr;
+          
+          cast_def cast_expr;
+          compound_def compound_expr;
+
+          func_call_def func_call_expr;
+     };
+};
+
+typedef struct StmtBlock {
+     size_t num_stmts;
+     BUF(Stmt **stmts); // Buffer to hold a list of statements
+} StmtBlock;
+
+
+typedef struct Elseif {
+     Expr *cond;
+     StmtBlock elseif_block;
+} Elseif;
+
+typedef struct stmt_if_def {
+     Expr *cond;  // the condition for if statement
+     StmtBlock if_block;
+     size_t num_elseifs;
+     Elseif *elseifs; // list of pointers to else_if blocks
+     StmtBlock else_block;
+} stmt_if_def;
+
+typedef struct stmt_assign_def { // Colon assign
+     const char *name;
+     Expr *rhs;
+} stmt_assign_def;
+
+typedef struct stmt_op_assign_def {
+     Expr *lhs;
+     TokenKind op;
+//     const char *name;
+     Expr *rhs;
+}  stmt_op_assign_def;
+
+typedef struct Case {
+     Expr **expr_list; // buffer to hold a list of expressions
+     size_t num_expr;
+     bool isdefault;
+     StmtBlock case_block;
+} Case;
+
+typedef struct stmt_switch_def{
+     Expr *expr; // Allows for multiple expression checking e.g case '1', 1 , 2 : ... 
+     size_t num_cases;
+    BUF( Case *cases;) // buffer to hold all the case statements;
+} stmt_switch_def;
+
+typedef struct stmt_for_def{
+     Expr *expr_init;
+     Expr *expr_update;
+     Expr *expr_cond;
+     StmtBlock block; // Statements block for for, while... statements, if block for if statement.
+} stmt_for_def;
+
+typedef struct stmt_return_def {
+     Expr *return_expr;
+} stmt_return_def;
+
+typedef struct stmt_while_def {
+     Expr *expr_cond;
+     StmtBlock block; // Statements block for for, while... statements, if block for if statement.
+} stmt_while_def;
+
+struct Stmt{
+     StmtKind kind;
+//     Expr *expr; //  for statements like for, while ,return, switch do while etc, cond expression for if .
+
+     union {
+          stmt_return_def return_stmt;
+          stmt_while_def while_stmt;
+                    
+          stmt_if_def if_stmt;
+          stmt_for_def for_stmt;
+          stmt_switch_def switch_stmt;
+          stmt_assign_def assign_stmt;
+          Expr *expr_stmt;
+          StmtBlock block;
+     };
+
+};
+
