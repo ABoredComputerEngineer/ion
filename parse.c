b4ab71ffab99e1c06ebc65ca0be219f31caacda9
Asim Maharjan <maharjan291@gmail.com>
2018-08-11 14:29:00 +0545

    Added parsing of sizeof exprs, changed from swtich to if statement, now gives error for blank statements

--- parse.c
+++ parse.c
@@ -4,8 +4,192 @@
 //
 StmtBlock parse_stmt_block();
 StmtBlock parse_stmt_list();
+TypeSpec *parse_type();
 Stmt *parse_stmt();
 Stmt *parse_stmt_if();
+Expr *parse_expr();
+Expr *parse_expr_test(){
+     return parse_expr();
+}
+
+
+Expr *parse_expr_operand(){
+     Expr *operand;
+      if  ( is_token(TOKEN_INT) ){
+               operand = expr_int(token.int_val); 
+               next_token();
+               return operand;
+      }else if ( is_token( TOKEN_FLOAT ) ){
+               operand = expr_float(token.float_val);
+               next_token();
+               return operand;
+      } else if ( is_token(TOKEN_STR) ){
+               operand = expr_str(token.str_val);
+               next_token();
+               return operand;
+      } else if ( is_token( TOKEN_NAME ) ){
+               const char *name = token.name;
+               next_token();
+               if ( match_token('{') ){
+                    Expr **expr_list = NULL;
+                    buff_push(expr_list,parse_expr());
+                    while ( !is_token('}') && !is_token(TOKEN_EOF) ){
+                         match_token(',');
+                         buff_push(expr_list,parse_expr());
+                    }
+                    expect_token('}');
+                    return expr_compound( type_name(name),expr_list,buff_len(expr_list));
+               } else {
+                    return expr_name(name);
+               }
+      } else if ( match_token('(') ){ 
+               Expr *new_expr = NULL;
+               if ( match_token(':') ){
+                    TypeSpec *type = parse_type();
+                    expect_token(')');
+                    new_expr = parse_expr();
+                    return expr_cast(type,new_expr);
+               }
+               new_expr = parse_expr();
+               expect_token(')');
+               return new_expr;
+          } else if ( match_token('{') ){
+                     Expr **list = NULL;
+                     buff_push(list,parse_expr());
+                     while ( !is_token(TOKEN_EOF)  && !is_token('}') ){
+                          match_token(',');
+                          buff_push(list,parse_expr());
+                          
+                     }
+                     expect_token('}');
+                   return expr_compound(NULL,list,buff_len(list));
+          } else if ( match_keyword("sizeof") ){
+               expect_token('(');
+               if ( match_token(':')){
+                   TypeSpec *type = parse_type();
+                  expect_token(')');
+                 return expr_sizeof_type(type); 
+               } else {
+                    Expr *expr = parse_expr();
+                    expect_token(')');
+                    return expr_sizeof_expr(expr);
+               }
+          }
+      syntax_error("Expected Expression but didn't get any thing");
+      return NULL;
+}
+
+Expr **parse_expr_list(){
+     Expr **list = NULL;
+     buff_push(list,parse_expr());
+     while ( !is_token(TOKEN_EOF) && match_token(',') ){
+          buff_push(list,parse_expr());
+     }
+     return list;
+}
+
+Expr *parse_expr_base(){
+     Expr *operand = parse_expr_operand();
+     if ( match_token('(') ){
+          Expr **expr_list = NULL;
+          buff_push(expr_list,parse_expr());
+          while ( !is_token(')') && !is_token(TOKEN_EOF) ){
+               match_token(',');
+               buff_push(expr_list,parse_expr());
+          }
+     //     Expr **expr_list = parse_expr_list(); 
+          expect_token(')');
+          operand = expr_call(operand,expr_list,buff_len(expr_list));
+     } else if ( match_token('[') ){
+          operand = expr_index(operand,parse_expr());
+          expect_token(']');
+     } else if ( match_token('.') ){
+          if ( is_token(TOKEN_STR) ){
+               operand = expr_field(operand,token.str_val);
+          } else {
+               syntax_error("Field access requires name to be final token\n");
+          }
+          next_token();
+     } 
+
+     return operand;
+}
+
+Expr *parse_expr_unary(){
+     Expr *unary_expr;
+     if ( is_unary_op() ){
+          TokenKind op = token.kind;
+          next_token();
+          unary_expr = expr_unary(op,parse_expr_unary());
+     } else {
+          unary_expr = parse_expr_base();
+     }
+     return unary_expr;
+}
+
+Expr *parse_expr_mul(){
+     Expr *mul_expr = parse_expr_unary();
+     while ( is_mul_op() ){
+          TokenKind op = token.kind;
+          next_token();
+          mul_expr = expr_binary(op,mul_expr,parse_expr_unary());
+     }
+     return mul_expr;
+}
+
+Expr *parse_expr_add(){
+     Expr *add_expr = parse_expr_mul();
+     while ( is_add_op() ){
+          TokenKind op = token.kind;
+          next_token();
+          add_expr = expr_binary(op,add_expr,parse_expr_mul());
+     }
+     return add_expr;
+}
+
+Expr *parse_expr_comp(){
+     Expr *comp_expr = parse_expr_add();
+     while ( is_comp_op() ){
+          TokenKind op = token.kind;
+          next_token();
+          comp_expr = expr_binary(op,comp_expr,parse_expr_add());
+     }
+     return comp_expr;
+}
+Expr *parse_expr_and(){
+     Expr *and_expr = parse_expr_comp();
+     while ( match_token(TOKEN_AND) ){
+          //TokenKind op = token.kind;
+          and_expr = expr_binary(TOKEN_AND,and_expr,parse_expr_comp());
+     }
+     return and_expr;
+}
+
+Expr *parse_expr_or(){
+     Expr *or_expr = parse_expr_and();
+     while ( match_token(TOKEN_OR) ){
+//          TokenKind op = token.kind;
+          or_expr = expr_binary(TOKEN_OR,or_expr,parse_expr_and());
+     }
+     return or_expr;
+}
+
+Expr *parse_expr_ternary(){
+     Expr *ternary_expr = parse_expr_or();
+     if ( match_token('?') ){
+          Expr *then_expr = parse_expr_ternary();
+          expect_token(':');
+          Expr *else_expr = parse_expr_ternary();
+          return expr_ternary(ternary_expr,then_expr,else_expr);
+     } else {
+          return ternary_expr;
+     }
+}
+
+Expr *parse_expr(){
+     return parse_expr_ternary();
+}
+
 const char *parse_name(){
      if ( is_token(TOKEN_NAME) ){
           const char *name = token.name;
@@ -47,8 +231,7 @@ TypeSpec *parse_type(){
      if ( match_token('*') ){
           type = type_pointer(type);
      } else if ( match_token('[') ){
-          //parse expr here
-          Expr *expr = expr_int(token.int_val);
+          Expr *expr = parse_expr();
           type = type_array(type,expr);
           expect_token(']');
      }
@@ -83,7 +266,7 @@ Decl *parse_func_decl(){
 }
 Decl *parse_typedef(){
      const char *name = parse_name();
-     expect_token('=');
+     expect_token(TOKEN_ASSIGN);
      TypeSpec *type;
      type = parse_type();
      return decl_typedef(name,type);
@@ -95,12 +278,9 @@ Decl *parse_typedef(){
 Decl *parse_const(){
      const char *name = parse_name();
 //     next_token();
-     expect_token('=');
-     Expr *expr = NULL;
-          if ( is_token(TOKEN_INT) ) // parse expr here
-               expr = expr_int(token.int_val);
-          else if ( is_token(TOKEN_NAME) )
-               expr = expr_name(token.name);     
+     expect_token(TOKEN_ASSIGN);
+     Expr *expr = parse_expr();
+     
      return decl_const(name,expr);
 }
 
@@ -111,11 +291,8 @@ Decl *parse_var(){
      if ( match_token(':') ){
          type = parse_type();// 
      }
-     if ( match_token('=') ){
-          if ( is_token(TOKEN_INT) ) // parse expr here
-               expr = expr_int(token.int_val);
-          else if ( is_token(TOKEN_NAME) )
-               expr = expr_name(token.name);     
+     if ( match_token(TOKEN_ASSIGN) ){
+          expr = parse_expr();
      }
      return decl_var(name,type,expr);     
 }
@@ -152,14 +329,11 @@ enum_item parse_enum_item(){
      if ( is_token(TOKEN_NAME) ){
           name = token.name;
           next_token();
-          if ( match_token('=') ){
-               if ( is_token(TOKEN_INT) ){ // to be replaced by actual parse_expr function
-                   expr = expr_int(token.int_val);
-               } else {
-                    syntax_error("Not valid Expression assignment for enum item");
-               }
-               next_token();
+          if ( match_token(TOKEN_ASSIGN) ){
+                   expr = parse_expr();
           }
+         //      next_token();
+          
      } else {
           syntax_error("Not valid name for enum");
      }
@@ -217,20 +391,16 @@ StmtBlock parse_stmt_block(){
      expect_token('}');
      return block;
 }
-Expr *parse_expr(){
-     next_token();
-     return expr_int(1);
-}
 Case parse_switch_case(){
      Case new_case = {0};
-     static bool is_default = false;
+     bool is_default = false;
      StmtBlock block;
      Stmt **stmt_list = NULL;
      Expr **expr_list = NULL;
      while ( match_keyword("case") ){ // allows for case 1: case 2: ...
-           buff_push(expr_list,parse_expr());
+           buff_push(expr_list,parse_expr_test());
            while ( match_token(',') ){
-                buff_push(expr_list,parse_expr());
+                buff_push(expr_list,parse_expr_test());
            }
            expect_token(':'); 
      }
@@ -256,8 +426,7 @@ Case parse_switch_case(){
 Stmt *parse_stmt_switch(){
      expect_token('(');
      //parse expr
-     Expr *expr = expr_name("a");
-     next_token();
+     Expr *expr = parse_expr();
      expect_token(')');
      expect_token('{');
      Case *case_list = NULL;
@@ -270,15 +439,14 @@ Stmt *parse_stmt_switch(){
 
 Stmt *parse_stmt_simple(){
      //parse expr
-     Expr *lhs = expr_name("abc");
-     next_token();
+     Expr *lhs = parse_expr();
      if ( match_token(TOKEN_COLON_ASSIGN) ){
           if ( lhs->kind != EXPR_NAME ){
                syntax_error(":= must be preceded by a name");
           }
           //parse expr
-          next_token();
-          return stmt_init(lhs->name,expr_int(1));
+         // next_token();
+          return stmt_init(lhs->name,parse_expr());
      } else if ( is_token(TOKEN_INC) || is_token(TOKEN_DEC) ){
           TokenKind op = token.kind;
           next_token();
@@ -286,22 +454,24 @@ Stmt *parse_stmt_simple(){
      } else if ( is_assign_op() ){
           TokenKind op = token.kind;
           next_token();
-          Expr *rhs= parse_expr();
+          Expr *rhs= parse_expr_test();
           return stmt_assign(op,lhs,rhs);
      } else {
-          return NULL;
+          if ( lhs->kind == EXPR_CALL ){
+               return stmt_expr(lhs);
+          }
+          syntax_error("Blank Statement has no use\n");
      }
 }
 Stmt *parse_stmt_for(){
      expect_token('(');
      Stmt *init = NULL;
-          init = parse_stmt_simple();
-          expect_token(';');
+     init = parse_stmt_simple();
+     expect_token(';');
      Expr *cond = NULL;
      if ( !is_token(';') ){
-        cond = expr_binary(TOKEN_EQ,expr_name("a"),expr_int(23)); 
+          cond = parse_expr();
      }
-     next_token();// remove
     expect_token(';');
     Stmt *update = NULL;
     if ( !is_token(')') ){
@@ -318,8 +488,7 @@ Stmt *parse_stmt_while(){
      expect_token('(');
      //parse expr
      
-      Expr *test_expr = expr_binary(EQ,expr_name("a"),expr_int(12));
-     Expr *cond = test_expr;
+     Expr *cond = parse_expr();
      expect_token(')');
      StmtBlock block = parse_stmt_block();
      return stmt_while(cond,block);
@@ -331,8 +500,7 @@ Stmt *parse_stmt_do_while(){
      expect_token('(');
      // parse expr
      
-     Expr *test_expr = expr_binary(EQ,expr_name("a"),expr_int(12));
-     Expr *cond = test_expr;
+     Expr *cond = parse_expr();
      expect_token(')'); 
      expect_token(';');
      return stmt_do_while(cond,block);
@@ -341,8 +509,7 @@ Stmt *parse_stmt_do_while(){
 Stmt *parse_stmt_if(){
      expect_token('(');
      // parse expr
-      Expr *test_expr = expr_binary(EQ,expr_name("a"),expr_int(12));
-     Expr *expr = test_expr;
+     Expr *expr = parse_expr();
      expect_token(')');
      StmtBlock if_block = parse_stmt_block();
      Elseif *elseifs = NULL;
@@ -353,7 +520,7 @@ Stmt *parse_stmt_if(){
           if ( match_keyword("if") ){
                expect_token('(');
                // parse expr
-               expr = test_expr;
+               expr = parse_expr();
                expect_token(')');
                block_elseif = parse_stmt_block();
                temp = (Elseif){expr,block_elseif};
@@ -369,12 +536,12 @@ Stmt *parse_stmt_if(){
 
 Stmt *parse_return_stmt(){
      // parse expr
-     Expr *expr = NULL;
-     if ( is_token(TOKEN_NAME) ){
-          expr = expr_name(parse_name());
-     } else if ( is_token(TOKEN_INT) ){
-          expr = expr_int(token.int_val);
-     }
+     Expr *expr = parse_expr();
+     //if ( is_token(TOKEN_NAME) ){
+     //     expr = expr_name(parse_name());
+     //} else if ( is_token(TOKEN_INT) ){
+     //     expr = expr_int(token.int_val);
+     //}
      expect_token(';');
      return  stmt_return(expr);
 }
@@ -412,26 +579,47 @@ void parse_test(){
      assert(is_token_keyword("typedef"));
      assert( !is_token_keyword("fucnthisshit"));
      char *parse_string[] = {
-          //"var x:(func (char,char):int) = 12",
-          //"enum abc { FUCK, THIS = 23, SHIT, YOU = 12, FUCKING= 14, ASS = 9}",
-          //"struct abc { x,y:int; z:char;} ",
-          //"union abc { x,y:int; z:char;} ",
-          //"var x:int = 1234",
-          //"var x:Vector",
-      //"var x:char = abc",
-      //"const x = 1234",
-      //"typedef cell = int32",
-//        "func abc ( a:int,b:char) : int {}",
-//        "func xyz ( a:int,b:char) {}",
-//        "func xyz ( ) {}",
-//        "func xyz ( ):char {if(){break;continue;}else if (){break;}else{continue;}}",
-//        "func xyz ( ):char {if(){break;continue;}else{break;}}",
-//        "func xyz ( ):char {if(){break;continue;}else if(){break;}}",
-//        "func foo(a:int,b:int):char{while(){if(){continue;}}}",
-//        "func do_while(a:char,b:char):int{do { continue; }while();}",
-//        "func foo (a:char*,b:size_t):char*{break;continue;}",
-//          "func foo (a:char,b:char):int{for(i:=0;x;i++){continue;}}", 
-            "func foo (a:char,b:char):int{for(i:=0;x;i++){switch ( a ){ case a,b,c:x+=1 ;break; case b:case c: break; default:break;}}" 
+          "var x:(func (char,char):int) = 12",
+          "enum abc { FUCK, THIS = 23, SHIT, YOU = 12, FUCKING= 14, ASS = 9}",
+          "struct abc { x,y:int; z:char;} ",
+          "union abc { x,y:int; z:char;} ",
+          "var x:int = 1234",
+          "var x:Vector",
+      "var x:char = abc",
+      "const x = 1234",
+     "typedef cell = int32",
+        "func abc ( a:int,b:char) : int {}",
+        "func xyz ( a:int,b:char) {}",
+        "func xyz ( ) {}",
+        "func xyz ( ):char {if(x!=2){break;continue;}else if (x!=2){break;}else{continue;}}",
+        "func xyz ( ):char {if(x!=2){break;continue;}else{break;}}",
+        "func xyz ( ):char {if(x!=2){break;continue;}else if(x!=2){break;}}",
+        "func foo(a:int,b:int):char{while(a>=2){if(x!=2){continue;}}}",
+        "func do_while(a:char,b:char):int{do { continue; }while(x!=2);}",
+        "func foo (a:char*,b:size_t):char*{break;continue;}",
+          "func foo (a:char,b:char):int{for(i:=0;x;i++){continue;}}", 
+            "func foo (a:char,b:char):int{for(i:=0;x;i++){switch ( o ){ case a,b,c:x+=1 ;break; case b:case c: break; default:break;}}}" ,
+         "func foo (a:int):int{ x:=x+2; }",
+         "func foo (a:int):int{ x = y==2 ? a : b(~x,*y); }"
+         " func foo (a:int *):int{ x <<= *x+2*3 ;}"
+         " func fact(n:int):int { trace(\"fact\"); if ( n== 0 ){return 1;} else { return n*fact(n-1);}}",
+         "func fact(n:int):int { p := 1; for(i:=1; i<=n; i++){p*=1;} return p;}",
+          " var foo = a?a&b + c<<d + e*f == +u-v-w + *g/h(x,y) + -i%k[x] && m<=n*(p+q/r) : 0",
+          " func f(x:int):bool{switch(x){case 0:case 1: return true; case 2: default: return false;}}",
+         "enum Color { RED = 3, GREEN, BLUE = 0}",
+         " var x = b==1?1+2:3-4",
+         "const pi = 3.14",
+         "struct Vector {x,y:float;}",
+          "var v : Vector = {1.0,-1.0}",
+          "union IntOrFloat { i:int; f:float;}",
+          "typedef Vectors = Vector[1+2]",
+          "var v:int = **x",
+          "func fact(x:int):int{while(x!=1){product*=x;x = x - 1 ;}}",
+          "const x = sizeof(:int)",
+          "var x:int = sizeof(3)",
+          //"var x = ",
+          "func fact(x:int):int{2;}"
+               
      };
      Decl *temp_decl; 
      for ( char **it =  parse_string; it!=parse_string+sizeof(parse_string)/sizeof(char *); it++ ){
